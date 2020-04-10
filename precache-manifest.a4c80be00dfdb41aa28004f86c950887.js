442c969e44c4abc920adcd903076d51f2eae7c10
priyanshu <priyanshu911@gmail.com>
2020-04-10 17:33:47 +0530

    Updates

---
+++ precache-manifest.a4c80be00dfdb41aa28004f86c950887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e522362f75389ba3fad877773cb97c64",
+    "url": "/search-ui/index.html"
+  },
+  {
+    "revision": "725862d12e2366cf1b1c",
+    "url": "/search-ui/static/css/main.92fefbfa.chunk.css"
+  },
+  {
+    "revision": "2b8eb135b9e7d81757a1",
+    "url": "/search-ui/static/js/2.ba01dfa6.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/search-ui/static/js/2.ba01dfa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725862d12e2366cf1b1c",
+    "url": "/search-ui/static/js/main.c3c47401.chunk.js"
+  },
+  {
+    "revision": "4c01298455716ca3ebc3",
+    "url": "/search-ui/static/js/runtime-main.34962305.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/search-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
