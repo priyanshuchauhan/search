802a9b97ed04b94d1bf202c8de4c4ea55b3d6db3
priyanshu <priyanshu911@gmail.com>
2020-04-12 05:49:05 +0530

    Updates

---
+++ precache-manifest.d50771591ce38420478595614ee6b5c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a92a253ffc0d09d2be948f8f6d42e2",
+    "url": "https://priyanshuchauhan.github.io/search/index.html"
+  },
+  {
+    "revision": "7def3de3940323c59416",
+    "url": "https://priyanshuchauhan.github.io/search/static/css/main.d3fae937.chunk.css"
+  },
+  {
+    "revision": "96e9cf4777acd0ceb89a",
+    "url": "https://priyanshuchauhan.github.io/search/static/js/2.edcb2ce1.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "https://priyanshuchauhan.github.io/search/static/js/2.edcb2ce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7def3de3940323c59416",
+    "url": "https://priyanshuchauhan.github.io/search/static/js/main.014b4037.chunk.js"
+  },
+  {
+    "revision": "0f7d70f75e72a78aacd5",
+    "url": "https://priyanshuchauhan.github.io/search/static/js/runtime-main.366a223e.js"
+  },
+  {
+    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
+    "url": "https://priyanshuchauhan.github.io/search/static/media/antoine-barres.dfbf459c.jpg"
+  },
+  {
+    "revision": "50f32ef6f0bbf82de00a706852f810db",
+    "url": "https://priyanshuchauhan.github.io/search/static/media/paella.50f32ef6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/search/precache-manifest.43698635542f8f246ba77b89c0296c2c.js"
+  "https://priyanshuchauhan.github.io/search/precache-manifest.d50771591ce38420478595614ee6b5c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/search/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://priyanshuchauhan.github.io/search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
